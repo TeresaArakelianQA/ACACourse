c6af86aa1c4589648b02b976b7f4d463644e5977
Shoghik Gasparyan <shoghikgasparyan@gmail.com>
2022-05-04 16:57:21 +0400

    to find the title name of the chrome page

--- src/test/java/ShoghikGasparyan/acaAutomationTest1.java
+++ src/test/java/ShoghikGasparyan/acaAutomationTest1.java
@@ -1,4 +1,28 @@
 package ShoghikGasparyan;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class acaAutomationTest1 {
-}
+
+        public static void main(String[] args) {
+            WebDriver driver = WebDriverManager.getInstance(ChromeDriver.class).create();
+            try {
+                driver.get("http://automationpractice.com/index.php");
+                WebElement element = driver.findElement(By.tagName("title"));
+                System.out.println("the title name is: " + element.getAttribute("text"));
+                assertEquals( "My Store", element.getAttribute("text"));
+
+            } finally {
+                driver.quit();
+            }
+        }
+    }
+
